04c10edff1c7a8c8c5776e8e322a4069969125e3
Marka Surendhar <marka.surendhar@zapcg.com>
2025-03-18 12:01:19 +0530

    fixed front-end build

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 // routes
 app.get("/api/v1/test", (req, res) => {
@@ -46,7 +46,7 @@ app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
